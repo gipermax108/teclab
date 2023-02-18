bd4cac181af8c90b7e0a61dfa52c210a2c724609
gipermax108 <115697857+gipermax108@users.noreply.github.com>
2023-02-18 12:48:27 +0500

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+int mygetch()
+{
+    struct termios oldt, newt;
+    int c;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    c = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return c;
+}
+
+int main()
+{
+    int state = 0; // состояние конечного автомата
+    char c; // вводимый символ
+    while ((c = mygetch()) != 4) { // считываем символы до нажатия End (код символа 4)
+        switch (state) {
+            case 0:
+                if (c >= '0' && c <= '9') {
+                    state = 1;
+                } else if (c == '+' || c == '-') {
+                    state = 2;
+                } else if (c == '.') {
+                    state = 3;
+                } else {
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case 1:
+                if (c >= '0' && c <= '9') {
+                    state = 1;
+                } else if (c == '.') {
+                    state = 4;
+                } else if (c == 'e' || c == 'E') {
+                    state = 5;
+                } else {
+                    printf("Valid\n");
+                    return 0;
+                }
+                break;
+            case 2:
+                if (c >= '0' && c <= '9') {
+                    state = 1;
+                } else if (c == '.') {
+                    state = 3;
+                } else {
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case 3:
+                if (c >= '0' && c <= '9') {
+                    state = 4;
+                } else {
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case 4:
+                if (c >= '0' && c <= '9') {
+                    state = 4;
+                } else if (c == 'e' || c == 'E') {
+                    state = 5;
+                } else {
+                    printf("Valid\n");
+                    return 0;
+                }
+                break;
+            case 5:
+                if (c >= '0' && c <= '9') {
+                    state = 7;
+                } else if (c == '+' || c == '-') {
+                    state = 6;
+                } else {
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case 6:
+                if (c >= '0' && c <= '9') {
+                    state = 7;
+                } else {
+                    printf("Error\n");
+                    return 0;
+                }
+                break;
+            case 7:
+                if (c >= '0' && c <= '9') {
+                    state = 7;
+                }
+            else {
+                printf("Valid\n");
+                return 0;
+            }
+            break;
+    }
+}
+return 0;
+}
--- lab2.c
+++ lab2.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <math.h>
-#define M_PI
 int main()
 {
     float x;
-    double M_PI;
     printf("Enter x { -1.0 <= x <= 1.0 } -> ");
     scanf("%f", &x);
     if (-1.0 <= x && x <= 1.0)
     {
-        float y = pow(sin(2*x),2) + pow(sin(3*x),2);
-        float z = pow(log(tan(y-M_PI / 8)),0.25);
+        float y = sqrt(sin(2*x)) + sqrt(sin(3*x));
+        float z = pow( log(tan(y-3.14/8)),1/4);
         printf("y(x) = %f\nz(y) = %f\n", y, z);
     }
     else
