a61d17e2dda95fa919e54a22cc3dea23fcd031e3
gipermax108 <115697857+gipermax108@users.noreply.github.com>
2023-04-18 15:05:01 +0300

    Add files via upload

---
+++ 2labarot.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MATH_dPI M_PI
+
+int main() {
+    float x;
+    printf("Enter x { -1.0 <= x <= 1.0 } -> ");
+    scanf("%f", &x);
+    if (-1.0 <= x && x <= 1.0) {
+        float y = sqrt(fabs(sin(2*x))) + sqrt(fabs(sin(3*x)));
+        float z = pow(log10(tan(y-(MATH_dPI / 8))), 0.25);
+        printf("y(x) = %f\nz(y) = %f\n", y, z);
+    } else {
+        printf("x value is incorrect!\n");
+    }
+    return 0;
+}
